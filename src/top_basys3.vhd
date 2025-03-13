d8d575efd9ccbd663a7f80fc246b19e006b424a9
brickeber <100965047+brickeber@users.noreply.github.com>
2025-03-13 15:00:26 -0600

    ICE 4 Final Sub 1

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -69,7 +69,17 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 
 --Declare stoplight component here 
-
+ component stoplight_fsm is
+    port (
+        i_C : in std_logic;
+        i_reset : in std_logic;
+        i_clk : in std_logic;
+        o_R : out std_logic;
+        O_Y : out std_logic;
+        O_G : out std_logic
+    );
+    end component;
+ 
 
 component clock_divider is
 	generic ( constant k_DIV : natural := 2	);
@@ -84,14 +94,22 @@ end component clock_divider;
 begin
 	-- PORT MAPS ----------------------------------------
 	--Port map stoplight here based on the design provided
-
+    stoplight_fsm_F : stoplight_fsm
+    port map(
+        i_C => sw(0),
+        i_reset => btnC,
+        i_clk => w_clk,
+        o_R => JA(0),
+        o_Y => JA(1),
+        o_G => JA(2)
+    );
 
 --Complete the clock_divider portmap below based on the design provided	
 	clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
         generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
         port map (						  
             i_clk   => clk,
-            i_reset => btnC,
+            i_reset => btnL,
             o_clk   => w_clk
         );    
 	
